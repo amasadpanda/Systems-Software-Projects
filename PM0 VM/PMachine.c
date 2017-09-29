b20750f73049829e1bda0d03ee815eb09b46a6c4
Timothy Yu <amasadpanda@gmail.com>
2017-09-29 17:08:10 -0400

    Finished most of the program. Next step is testing the program on eustis.

--- PM0 VM/PMachine.c
+++ PM0 VM/PMachine.c
@@ -5,6 +5,10 @@
 #define MAX_CODE_LENGTH 500
 #define MAX_LEXI_LEVELS 3
 
+// The output can only store MAX_OUTPUT_LENGTH integers
+// aka, can only call SIO 1 this many times
+#define MAX_OUTPUT_LENGTH 20
+
 typedef struct
 {
 	int op;
@@ -16,13 +20,16 @@ typedef struct
 const char* INST_NAME[23] = {"LIT", "RTN", "LOD", "STO", "CAL", "INC", "JMP", "JPC", "SIO"
 									, "NEG", "ADD", "SUB", "MUL", "DIV", "ODD", "EQL", "NEQ", "LSS", "LEQ", "GTR", "GEQ"};
 
-int REGISTERS[8];
-int STACK[MAX_STACK_HEIGHT] = {0};
+int 			REGISTERS[8];
+int 			STACK[MAX_STACK_HEIGHT] = {0};
+int			DIVIDER[MAX_STACK_HEIGHT/4] = {0};
 instruction CODE[MAX_CODE_LENGTH];
-int RUN = 0;
 
 instruction IR;
-int BP, SP, PC = 0;
+int BP = 1, SP = 0, PC = 0, RUN = 0, STACK_COUNT = 1;
+
+int OUTPUT[MAX_OUTPUT_LENGTH];
+int OUTCOUNTER = 0;
 
 int base(int l, int base)
 {
@@ -47,6 +54,7 @@ int execute(instruction exec)
 		case 2: SP = BP-1;
 				  BP = STACK[SP+3];
 				  PC = STACK[SP+4];
+				  STACK_COUNT--;
 			break;
 		// LOD, loads values from STACK to register
 		case 3: REGISTERS[exec.r] = STACK[base(exec.l, BP) + exec.m];
@@ -59,6 +67,8 @@ int execute(instruction exec)
 				  STACK[SP+2] = base(exec.l, BP);
 				  STACK[SP+3] = BP;
 				  STACK[SP+4] = PC;
+				  DIVIDER[STACK_COUNT] = SP;
+				  STACK_COUNT++;
 				  BP = SP+1;
 				  PC = exec.m;
 			break;
@@ -76,11 +86,16 @@ int execute(instruction exec)
 			break;
 		// SIO, print out REGISTERS, red in REGISTERS, set halt flag
 		case 9: if(exec.m == 1)
-					printf("%d\n", REGISTERS[exec.r]);
+					// Instead of outputing directly to printf, output is stored inside an array then
+					// outputted at the very end.
+					OUTPUT[OUTCOUNTER++] = REGISTERS[exec.r];
 				  else if(exec.m == 2)
 				  	scanf("%d", &REGISTERS[exec.r]);
 				  else if(exec.m == 3)
-				  	RUN = 0;
+				  {
+				 	RUN = 0;
+				 	SP = 0;
+				  }
 			break;
 		// NEG
 		case 10: REGISTERS[exec.r] = 0 - REGISTERS[exec.l];
@@ -126,14 +141,14 @@ int execute(instruction exec)
 
 void printInstruct(instruction instr, int codeLine)
 {
-	printf("%d\t%s %d\t%d\t%d\t%d\t%d\t%d\n", codeLine, INST_NAME[instr.op], instr.r, instr.l, instr.m, PC, BP, SP);
+	printf("%d %s %d %d %d\t%d\t%d\t%d\t", codeLine, INST_NAME[instr.op-1], instr.r, instr.l, instr.m, PC, BP, SP);
 
 }
 
 void printCode()
 {
 	int i = 0;
-	printf("Program Printout\n\n");
+	printf("Printout\n\n");
 	while(CODE[i].op != 0)
 	{
 		printf("  %d %s %d %d %d\n", i, INST_NAME[CODE[i].op], CODE[i].r, CODE[i].l, CODE[i].m);
@@ -142,17 +157,43 @@ void printCode()
 	printf("\n");
 }
 
+void printStack()
+{
+	int i;
+	int stackcount = 0;
+	for(i = 0; i <= SP; i++)
+	{	
+		printf("%d", STACK[i]);
+		if(DIVIDER[stackcount] == i)
+		{
+			printf(" | ");
+			stackcount++;	
+		}
+	}
+}
+
+void printRegisters()
+{
+	printf("\n\t\t\t\t\t[%d][%d][%d][%d][%d][%d][%d][%d]\n", REGISTERS[0], REGISTERS[1], REGISTERS[2], REGISTERS[3],
+			 REGISTERS[4], REGISTERS[5], REGISTERS[6], REGISTERS[7]);
+}
+
 int main(int argc, char *argv[])
 {
 
-	/*FILE *ifp = NULL;
-	int *buffer = NULL
+	FILE *ifp = NULL;
+	int *buffer = NULL;
 	size_t len;
 	int count = 0;
+	int i;
 
+	if(argc < 2)
+	{
+		fprintf(stderr, "Correct syntax for this program: <filename>\n");
+		exit(1);
+	}
 	ifp = fopen(argv[1],"r");
-
-	if (ifp == NULL)
+	if(ifp == NULL)
 	{
 			fprintf(stderr, "File %s not found.\n", argv[1]);
 			exit(1);
@@ -160,35 +201,36 @@ int main(int argc, char *argv[])
 	else
 	{
 			//prints header first before pulling data out of txt file
-			printf("Factorial Program:\n");
+			printf("%s Program:\n", argv[1]);
 			while(!feof(ifp))
 			{
-					fscanf(ifp,"%d",&code[count].op);
-					fscanf(ifp, "%d", &code[count].r)
-					fscanf(ifp,"%d", &code[count].l)
-					fscanf(ifp, "%d", &code[count].m));
+					fscanf(ifp,"%d", &CODE[count].op);
+					fscanf(ifp,"%d", &CODE[count].r);
+					fscanf(ifp,"%d", &CODE[count].l);
+					fscanf(ifp,"%d", &CODE[count].m);
 					count++;
 			}
-	}*/
+	}
 
 
 	RUN = 1;
-	CODE[0].op = 1; CODE[0].r = 0; CODE[0].m = 123;
-	CODE[1].op = 9; CODE[1].r = 0; CODE[1].m = 1;
-	CODE[2].op = 9; CODE[2].r = 1; CODE[2].m = 2;
-	CODE[3].op = 9; CODE[3].r = 1; CODE[3].m = 1;
-	CODE[4].op = 9; CODE[4].r = 0; CODE[3].m = 3;
 
 	printCode();
-	printf("Program Stack Trace\nInstructions\t\t\tPC\tBP\tSP\n");
+	printf("Stack Trace\nInstructions\tPC\tBP\tSP\tStack|Registers\n-------------------------------------------------\n");
 	while(RUN && PC < MAX_CODE_LENGTH)
 	{
 		IR = CODE[PC];
 		PC++;
 
-		printInstruct(IR, PC-1);
+		
 		execute(IR);
+		printInstruct(IR, PC-1);
+		printStack();
+		printRegisters();
 
 	}
+	printf("%s Output:\n", argv[1]);
+	for(i = 0; i < OUTCOUNTER; i++)
+		printf("%d\n", OUTPUT[i]);
 
 }
