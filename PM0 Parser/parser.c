788c148e8cdb97002f27dae9d3adfaf0006d7312
amasadpanda <amasadpanda@gmail.com>
2017-11-03 14:41:04 -0400

    finished parser, need to add code generation

--- PM0 Parser/parser.c
+++ PM0 Parser/parser.c
@@ -1,24 +1,18 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 #include <ctype.h>
 #include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define MAX_SYMBOL_TABLE_SIZE 1000
 #define CODE_SIZE 1000
-#define MAX_LEX_LEVELS 3
 #define MAX_CHAR 11
 #define MAX_DIGIT 5
+#define MAX_LEX_LEVELS 3
+#define MAX_SYMBOL_TABLE_SIZE 1000
 
 typedef enum
 {
-
-   nulsym = 1, identsym, numbersym, plussym, minussym,
-   multsym, slashsym, oddsym, eqsym, neqsym, lessym, leqsym,
-   gtrsym, geqsym, lparentsym, rparentsym, commasym, semicolonsym,
-   periodsym, becomessym, beginsym, endsym, ifsym, thensym,
-   whilesym, dosym, callsym, constsym, varsym, procsym, writesym,
-   readsym , elsesym
+   nulsym, identsym, numbersym, plussym, minussym, multsym, slashsym, oddsym, eqsym, neqsym, lessym, leqsym, gtrsym, geqsym, lparentsym, rparentsym, commasym, semicolonsym, periodsym, becomessym, beginsym, endsym, ifsym, thensym, whilesym, dosym, callsym, constsym, varsym, procsym, writesym, readsym, elsesym
 } token_type;
 
 //Contains all tokens
@@ -31,82 +25,352 @@ typedef struct
 
 //Data structure for the symbol
 typedef struct
-    {
-	       int kind; 	// const = 1, var = 2, proc = 3
-	       char name[10];	// name up to 11 chars
-	       int val; // number (ASCII value)
-	       int level; // L level
-	       int addr; // M address
-    }symbol;
+{
+  int kind; 	// const = 1, var = 2, proc = 3
+  char name[10];	// name up to 11 chars
+  int val; // number (ASCII value)
+  int level; // L level
+  int addr; // M address
+}symbol;
 
 typedef struct
 {
     int op; //Opcode
-    int l; //L
-    int m; //M
+    int r;  // register
+    int l;  //L
+    int m;  //M
 }instruction;
 
-symbol_table[MAX_SYMBOL_TABLE_SIZE]  = {0};
-
+instruction CODE_TABLE[CODE_SIZE];
+symbol symbol_table[MAX_SYMBOL_TABLE_SIZE];
 /* Constants stored by kind, name, and value.
    Variables stored by kind, name, L and M.
    Procedures stored by kind, name, L and M.
 */
 
 //Global Variables
-int token, codeindex, num, kind, count = 0, lexemeIndex = 0, char id[12];
+int TOKEN, NUM, KIND, CODEINDEX = 0, SYMBOLCOUNT = 0, LEXEMEINDEX = 0, DATAINDEX = 4;
+char ID[12];
+tokenStruct lexemeList;
 
 //functions
 void errors(int errorCases);
 void parser(FILE *ifp, symbol *table, instruction *code);
 void gen(int op, int l, int m, instruction *code);
 int nextToken(FILE *ifp);
+int getToken(FILE *ifp);
 
-
-void main(void)
+// Gets the list of lexeme from a file, stores the tokens and returns how many tokens are found.
+int lexemeList(File* lexemeFile)
 {
-    FILE *ifp = fopen("output.txt","r");
-    FILE *ofp = fopen("parseroutput.txt", "w");
-	
+  lexemeList = malloc(sizeof(tokenStruct) * 300);
+  int count = 0;
+  while(!feof(lexemFile))
+  {
+    fscanf(lexemeFile, "%d", &lexemeList[count].tokenID);
+    if(lexemeList[count].tokenID == 2)
+      fscanf(lexemeFile, "%s", &lexemeList[count].name);
+    else if(lexemeList[count].tokenID == 3)
+      fscanf(lexemeFile, "%d", &lex1emeList[count].numval);
+    count++;
+  }
+
+  return count;
+}
+
+tokenStruct getToken()
+{
+  TOKEN = lexemeList[LEXEMEINDEX].tokenID;
+	if (token == 2)
+	{
+	   strcpy(ID, lexemeList[++LEXEMEINDEX].name);
+	}
+	else if (token == 3)
+	{
+      NUM = lexemeList[++LEXEMEINDEX].numval;
+	}
+	LEXEMEINDEX++;
+	return token;
+}
+
+int relationalSymbol()
+{
+  switch(TOKEN)
+  {
+    case token_type.eqsym: return 1;
+    case token_type.neqsym: return 1;
+    case token_type.lessym: return 1;
+    case token_type.leqsym: return 1;
+    case token_type.gtrsym: return 1;
+    case token_type.geqsym: return 1;
+    default: return 0;
+  }
+  return 0;
+}
+
+void generateVar()
+{
+  symbol_table[SYMBOLCOUNT].kind = 2;
+  // is this allowed
+  strcpy(symbol_table[SYMBOLCOUNT].name, NAME);
+  symbol_table[SYMBOLCOUNT].level = 0;
+  symbol_table[SYMBOLCOUNT].addr = DATAINDEX;
+
+  SYMBOLCOUNT++;
+  DATAINDEX++;
+}
+
+void generateNum()
+{
+  symbol_table[SYMBOLCOUNT].kind = 3;
+  symbol_table[SYMBOLCOUNT].value = NUM;
+  symbol_table[SYMBOLCOUNT].level = 0;
+  symbol_table[SYMBOLCOUNT].addr = DATAINDEX;
+
+  SYMBOLCOUNT++;
+  DATAINDEX++;
+}
+
+void generateCode(int opcode, int r, int l, int m)
+{
+  if(CODEINDEX > CODE_SIZE)
+    error(27);
+
+  CODE_TABLE[CODEINDEX].op = opcode;
+  CODE_TABLE[CODEINDEX].r = r;
+  CODE_TABLE[CODEINDEX].l = l;
+  CODE_TABLE[CODEINDEX].m = m;
+
+  CODEINDEX++;
+
+}
+
+int findVar()
+{
+  int i;
+  for(i = 0; i < SYMBOLCOUNT; i++)
+  {
+    if(strcmp(symbol_table[i].name, NAME))
+      return i;
+  }
+  return -1;
+}
+
+/*
+##############################################
+  Below is the code pertaining to the parser
+##############################################
+*/
+void factor()
+{
+  if(TOKEN == token_type.identsym)
+  {
+    if(findVar() < 0)
+      error();
+    getToken();
+  }
+  else if(TOKEN == token_type.numbersym)
+  {
+    getToken();
+  }
+  else if(TOKEN == token_type.lparentsym)
+  {
+    getToken();
+    expression();
+    if(TOKEN != token_type.rparentsym)
+      error();
+    getToken();
+  }
+  else
+  {
+    error();
+  }
+}
+
+void term();
+{
+  factor();
+  while(TOKEN == token_type.multsym || TOKEN == token_type.slashsym)
+  {
+    getToken();
+    factor();
+  }
+}
+
+void expression()
+{
+  if(TOKEN == token_type.plussym || TOKEN == token_type.minussym)
+    getToken();
+  term();
+  while(TOKEN == token_type.plussym || TOKEN == token_type.minussym)
+  {
+    getToken();
+    term();
+  }
+}
+
+void condition()
+{
+  if(TOKEN == token_type.oddsym)
+  {
+    getToken();
+    expression();
+  }
+  else
+  {
+    expression();
+    if(!relationalSymbol)
+      error();
+    getToken();
+    expression();
+  }
+}
+
+void statement()
+{
+  if(TOKEN == token_type.identsym)
+  {
+    int place = findVar();
+    getToken();
+    if(TOKEN != token_type.becomessym)
+      error();
+    getToken();
+    expression();
+    generateCode(4, 1, 0, symbol_table[place].addr);
+  }
+  else if(TOKEN == token_type.beginsym)
+  {
+    getToken();
+    statement();
+    while(TOKEN == token_type.semicolonsym)
+    {
+      getToken();
+      statement();
+    }
+    if(TOKEN != token_type.endsym)
+      error();
+    getToken();
+  }
+  else if(TOKEN == token_type.ifsym)
+  {
+    getToken();
+    condition();
+    int f = CODEINDEX;
+    generateCode(8, 0, 0, CODEINDEX);
+    if(TOKEN != token_type.thensym)
+      error();
+    getToken();
+    statement();
+    CODE_TABLE[f].m = CODEINDEX;
+  }
+  else if(TOKEN == token_type.whilesym)
+  {
+    getToken();
+    condition();
+    int f = CODEINDEX;
+    generateCode(8, 0, 0, CODEINDEX);
+    if(TOKEN != token_type.dosym)
+      error();
+    getToken();
+    statement();
+    CODE_TABLE[f].m = CODEINDEX;
+  }
+}
+
+void block()
+{
+  if(TOKEN == token_type.constsym)
+  {
+    while(true)
+    {
+      getToken();
+      if(TOKEN != token_type.identsym)
+        error();
+
+      int placeInTable = findVar();
+      // cannot be found
+      if(placeInTable < 0)
+        error();
+
+      getToken();
+      if(TOKEN != token_type.eqsym)
+        error();
+      getToken();
+      if(TOKEN != token_type.numbersym)
+        error();
+      symbol_table[placeInTable] = NUM;
+      getToken();
+      if(TOKEN != token_type.commasym)
+        break;
+    }
+    if(TOKEN != token_type.semicolonsym)
+      error();
+    getToken();
+  }
+  if(TOKEN == token_type.varsym)
+  {
+    while(true)
+    {
+      getToken();
+      if(TOKEN != token_type.identsym)
+        error();
+      generateVar();
+      getToken();
+      if(TOKEN != token_type.commasym)
+        break;
+    }
+    if(TOKEN != token_type.semicolonsym)
+      error();
+    getToken();
+  }
+  statement();
+}
+
+void program()
+{
+  getToken()
+  block();
+  if(TOKEN != token_type.periodsym)
+    error(9);
+}
+
+void parser(FILE *ifp, symbol *table, instruction *code)
+{
+   TOKEN = getToken(ifp);
+}
+
+void main(int n, char** args)
+{
+    FILE* ifp = fopen("output","r");
+    FILE* ofp = fopen("assembly", "w");
+
+    for(int i = 0; i < n; i++)
+      printf("%s", args[i]);
+
     int i, lexlev = 0, dataindex = 0, tableindex = 0;
 
     instruction code[CODE_SIZE];
 
     //calls function parser that runs program
     parser(ifp, table, code);
-    for(i=0; i<codeindex; i++)
+    for(i=0; i<CODEINDEX; i++)
     {
-	fprintf(ofp, "%d %d %d\n", code[i].op, code[i].l, code[i].m);
+	     fprintf(ofp, "%d %d %d\n", code[i].op, code[i].l, code[i].m);
     }
     fclose(ifp);
     fclose(ofp);
 }
 
-void parser(FILE *ifp, symbol *table, instruction *code)
+void error(int errorCase)
 {
-   token = getToken(ifp);
-   
-
-
-}
-
-void gen(int op, int l, int m, instruction *code)
-{
-	
-}
-
-void errors(int errorCases)
-{
-    count++;
-
     switch (errorCases)
     {
         case 1:
             printf("Error 1: Use = instead of :=.\n");
             break;
-        case 2
-            printf("Error 2: = must be followed by a number.\n")
-	    break;
+        case 2:
+            printf("Error 2: = must be followed by a number.\n");
+	          break;
         case 3:
             printf("Error 3: Identifier must be followed by =.\n");
             break;
@@ -179,23 +443,11 @@ void errors(int errorCases)
         case 26:
             printf("Error 26: Level is larger than the maximum allowed lexicographical levels.\n");
             break;
+        case 27:
+            printf("Error 27: Generating too much code!\n");
+            break;
         default:
             break;
     }
     exit(1);
-}
-
-int getToken(FILE *ifp)
-{
-	token = lexList[lexemeIndex].tokenID;
-	if (token == 2)
-	{
-	   strcpy(id, lexList[lexemeIndex].name);
-	}
-	else if (token == 3)
-	{
-           num = lexList[lexemeIndex].numval;
-	}
-	lexemeIndex++;
-	return token; 
 }
