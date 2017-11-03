d6e6445f0980cda2f6bcf3633a5bd37439e8efe0
calebm120 <32340985+calebm120@users.noreply.github.com>
2017-11-03 19:16:26 -0400

    Create pl0data.h
    should make connection between 3 .c files easier and reduce code lines.

---
+++ PM0 Compiler/pl0data.h
@@ -0,0 +1,49 @@
+#define MAX_SYMBOL_TABLE_SIZE 1000
+#define CODE_SIZE 1000
+#define MAX_LEXI_LEVELS 3
+
+//variables
+
+extern int lexout;
+extern int parserout;
+extern int vmout;
+extern int count;
+
+//Given enum
+typedef enum {
+    nulsym = 1, identsym, numbersym, plussym, minussym,
+    multsym,  slashsym, oddsym, eqsym, neqsym, lessym, leqsym,
+    gtrsym, geqsym, lparentsym, rparentsym, commasym, semicolonsym,
+    periodsym, becomessym, beginsym, endsym, ifsym, thensym,
+    whilesym, dosym, callsym, constsym, varsym, procsym, writesym,
+    readsym , elsesym
+} token_type;
+
+//struct used to contain all tokens details
+typedef struct {
+    token_type tokenID;
+    int numval;
+    char name[12];
+}tokenStruct;
+
+extern tokenStruct lexList[5000];
+extern int lexemeIndex;
+
+ typedef struct {
+     int op; //Opcode
+     int l;  //L
+     int m;  //M
+ } instruction;
+
+extern instruction code[CODE_SIZE];
+
+
+//Functions shared throughout 3 .c files.
+
+//found and called in main.c
+void compile(void);
+
+//called in main.c. These are the 3 functions (.c files) called during execution.
+void Scanner(void);
+void parser(void);
+void PMachine(void);
